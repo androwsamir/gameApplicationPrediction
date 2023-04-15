f1fbe807b12cf6a664ec9acfa80d33ffeacb92a9
Andrew Ibrahim <androwsamir2019@gmail.com>
2023-04-15 20:21:42 +0200

    split data for train & test

---
+++ Preprocessing.py
@@ -0,0 +1,11 @@
+
+
+
+class Preprocess:
+
+    # Constructor
+    def __init__(self, x_train, x_test, y_train, y_test):
+        self.xTrain = x_train
+        self.xTest = x_test
+        self.yTrain = y_train
+        self.yTest = y_test
---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import Preprocessing
+
+def preprocess_input(filename):
+
+    # Read data from CSV file to dataset
+    dataset = pd.read_csv(filename)
+    # split all column except last one for input in X
+    X = dataset.iloc[:,:-1]
+    # split last column for output in Y
+    Y = dataset.iloc[:,-1]
+
+    return X, Y
+
+
+if __name__=='__main__':
+
+    # Get input in X & output in Y
+    X, Y = preprocess_input('games-regression-dataset.csv')
+
+    # Splitting the X,Y into the Training set and Test set
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+    # Create Object of Preprocess Class
+    preprocess = Preprocessing.Preprocess(x_train, x_test, y_train, y_test)
