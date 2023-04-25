ab6d39792f333dd6714bcc07507e4c410a659d36
Rehab Yousef <85287016+rehab14@users.noreply.github.com>
2023-04-25 20:05:07 +0200

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,5 +1,7 @@
 
-
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
 
 class Preprocess:
 
@@ -9,3 +11,20 @@ class Preprocess:
         self.xTest = x_test
         self.yTrain = y_train
         self.yTest = y_test
+        
+        
+    def pre_lbl_encoding( data , colums):
+        for x in colums:
+            le = LabelEncoder()
+            le.fit(list(data[x].values))
+            data[x] = le.transform(list(data[x].values))
+        return data
+
+    def pre_hot_encoding( data , colums):
+        for x in colums:
+            enc = OneHotEncoder()
+            enc.fit(list(list(data[x].values)))
+            data[x] = enc.transform(list(list(data[x].values)))
+        return data
+        
+    
